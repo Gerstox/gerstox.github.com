22c333fe9fd9f02896447c71c1dd363161415526
Gerson Pineda <Gerstox@users.noreply.github.com>
2020-07-05 22:53:00 -0400

    Ajustes de estilo y efectos

--- cv/assets/js/main.js
+++ cv/assets/js/main.js
@@ -1,6 +1,7 @@
 
 
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen-v").click();
 
 function openCompetence(evt, compentence) {
     // Declare all variables
@@ -20,5 +21,27 @@ function openCompetence(evt, compentence) {
   
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(compentence).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+  function openC(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent-v");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks-v");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
